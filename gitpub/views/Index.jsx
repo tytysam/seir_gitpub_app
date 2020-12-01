408271dde5f0cff3ca6bdcf9a67d57feaa2b11f2
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-30 20:00:03 -0700

    Index.jsx is showing drink data (with first letter of each drink name capitalized)

--- gitpub/views/Index.jsx
+++ gitpub/views/Index.jsx
@@ -4,9 +4,27 @@ const myStyle = {
   backgroundColor: "#000000",
 };
 
+const capitalize = () => {
+  return drink.name.charAt(0);
+};
+
 class Index extends React.Component {
   render() {
-    return <h1 style={myStyle}>Welcome to gitPub</h1>;
+    return (
+      <div>
+        <h1 style={myStyle}>Welcome to gitPub</h1>
+        <ul>
+          {this.props.drinks.map((drink) => {
+            console.log(drink);
+            return (
+              <li>
+                {drink.name.charAt(0).toUpperCase() + drink.name.slice(1)}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
